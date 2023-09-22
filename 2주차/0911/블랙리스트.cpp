830ab3a00f7d4cb5363b230b9ed6ad54d289ac08
Stella <88418078+pastel-blue@users.noreply.github.com>
2023-09-22 10:19:39 +0900

    사원 출입 관리 시스템
    PRO 레벨 테스트

--- 2주차/0911/블랙리스트.cpp
+++ 2주차/0911/블랙리스트.cpp
@@ -1,36 +1,133 @@
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include<string>
+#include<cstring> // memset, memcpy
 using namespace std;
 
-int main() {
-	int apt[100001] = { 0, };
-	int check[100001] = { 0, };
+string names[10000]; // names[5] = "Jack" -> id가 5면 Jack
+bool isRegister[10000]; // 특정 id가 사용중인가
+bool isIn[10000]; 
+/**user.cpp**/
+void init()
+{
+    for (int i = 0;i < 10000;i++) names[i] = "";
+    // primitive 타입에 대해서 memset, memcpy
+    memset(isRegister, 0, sizeof(isRegister)); // 배열이름은 배열의 주소값(포인터)
+    memset(isIn, 0, sizeof(isIn));
+}
 
-	int c, r;
-	cin >> c >> r;
-	for (int row = 0; row < r;row++) {
-		for (int col = 0;col < c;col++) {
-			int val;
-			cin >> val;
-			apt[val] += 1; // 아파트에 해당 번호 얼마나 있는지 체크
-		}
-	}
+std::string registerID(int id, std::string name)
+{
+    if (isRegister[id]) {
+        return "ERROR";
+    }
+    isRegister[id] = true;
+    isIn[id] = false;
+    names[id] = name;
+    return "OK";
+}
 
+std::string* inout(int id)
+{
+    std::string* res = new std::string[2];
+    if (!isRegister[id]) {
+        res[0] = "ERROR";
+        res[1] = "ERROR";
+        return res;
+    }
+    res[0] = names[id];
+    if (isIn[id]) {
+        isIn[id] = false;
+        res[1] = "OUT";
+    }
+    else {
+        isIn[id] = true;
+        res[1] = "IN";
+    }
+    return res;
+}
 
-	int blackCnt = 0;
-	int bc, br;
-	cin >> br >> bc;
-	for (int row = 0; row < br; row++) {
-		for (int col = 0; col < bc;col++) {
-			int n;
-			cin >> n; // 블랙리스트 번호
-			if (check[n] == 0) {
-				blackCnt += apt[n];
-				check[n] = 1; 
-			}
-		}
-	}
+/*** main.cpp ***/
+#include <stdio.h>
+#include<iostream>
+#include <string>
+#include <string.h>
 
-	cout << blackCnt << "\n" << (r * c) - blackCnt;
+#define MAX_N 100
 
-	return 0;
+extern void init();
+extern std::string registerID(int id, std::string name);
+extern std::string* inout(int id);
+
+#define CMD_REGISTER 1
+#define CMD_INOUT 2
+
+static bool run()
+{
+    int query_num;
+    scanf("%d", &query_num);
+
+    init();
+
+    std::string userAns1;
+    std::string* userAns2;
+    std::string ans1;
+    std::string ans2;
+    std::string name;
+    int id;
+    char tmp[100];
+    char tmp2[100];
+    bool ok = true;
+
+    for (int q = 0; q < query_num; q++)
+    {
+        int query;
+        scanf("%d", &query);
+
+        if (query == CMD_REGISTER)
+        {
+            scanf("%d %s", &id, tmp);
+            name = tmp;
+            userAns1 = registerID(id, name);
+
+            scanf("%s", tmp);
+            ans1 = tmp;
+
+            if (ans1 != userAns1)
+                ok = false;
+        }
+        else if (query == CMD_INOUT)
+        {
+            scanf("%d", &id);
+
+            userAns2 = inout(id);
+
+            scanf("%s %s", tmp, tmp2);
+
+            ans1 = tmp;
+            ans2 = tmp2;
+
+            if (ans1 != userAns2[0] || ans2 != userAns2[1])
+                ok = false;
+        }
+    }
+    return ok;
+}
+
+int main()
+{
+    setbuf(stdout, NULL);
+    freopen("input.txt", "r", stdin);
+
+    int T, MARK;
+    scanf("%d %d", &T, &MARK);
+
+    for (int tc = 1; tc <= T; tc++)
+    {
+        int score = run() ? MARK : 0;
+        printf("#%d %d\n", tc, score);
+    }
+    return 0;
 }
